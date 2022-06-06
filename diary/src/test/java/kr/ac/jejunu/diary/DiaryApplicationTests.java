42810ed14ecb733e86e91b055bfed15b1003637e
caffeinejun <gurwns232e@gmail.com>
2022-06-06 19:13:40 +0900

    프로젝트 생성 및 초기 설정

---
+++ diary/src/test/java/kr/ac/jejunu/diary/DiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
