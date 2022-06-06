c681d2ae1faff7e54593489026e7c2e1634fb24e
caffeinejun <gurwns232e@gmail.com>
2022-06-06 20:34:52 +0900

    회원 중복 시 회원가입 실패

--- diary/src/main/java/kr/ac/jejunu/diary/repository/UserRepository.java
+++ diary/src/main/java/kr/ac/jejunu/diary/repository/UserRepository.java
@@ -3,5 +3,8 @@ package kr.ac.jejunu.diary.repository;
 import kr.ac.jejunu.diary.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUserId(String userId);
 }
--- diary/src/main/java/kr/ac/jejunu/diary/service/UserService.java
+++ diary/src/main/java/kr/ac/jejunu/diary/service/UserService.java
@@ -9,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -17,10 +19,18 @@ public class UserService {
 
     //회원정보를 저장하고 비밀번호는 BCrypt라이브러리를 이용하여 해싱
     public ResponseDto signUp(UserSignRequestDto userSignRequestDto) {
+        checkUser(userSignRequestDto);
         User user=new User();
         user.setUserId(userSignRequestDto.getUserId());
         user.setPassword(BCrypt.hashpw(userSignRequestDto.getPassword(),BCrypt.gensalt()));
         userRepository.save(user);
         return new ResponseDto(202,"회원가입이 완료되었습니다.");
+    }
+
+    private void checkUser(UserSignRequestDto userSignRequestDto) {
+        Optional<User> byUserId = userRepository.findByUserId(userSignRequestDto.getUserId());
+        if(byUserId.isPresent()){
+            throw new IllegalArgumentException("회원가입 실패");
+        }
     }
 }
