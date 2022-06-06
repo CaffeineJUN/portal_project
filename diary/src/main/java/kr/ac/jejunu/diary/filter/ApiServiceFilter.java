5ed0ad7d5337a712fb0e10f0d51ab2ee1ffda062
caffeinejun <gurwns232e@gmail.com>
2022-06-06 21:33:32 +0900

    필터링 작업

--- diary/src/main/java/kr/ac/jejunu/diary/filter/ApiServiceFilter.java
+++ diary/src/main/java/kr/ac/jejunu/diary/filter/ApiServiceFilter.java
@@ -1,2 +1,35 @@
-package kr.ac.jejunu.diary.filter;public class ApiServiceFilter {
+package kr.ac.jejunu.diary.filter;
+
+import kr.ac.jejunu.diary.domain.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Slf4j
+public class ApiServiceFilter extends OncePerRequestFilter  {
+    private final String[] whitelist={"/","/api/login","/api/logout","/api/register"};
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        if(isWhiteList(requestURI)){
+            HttpSession session = request.getSession(false);
+            User user = (User)session.getAttribute("user");
+            if(user==null){
+                response.sendError(403,"접근권한이 없습니다.");
+                return;
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    private boolean isWhiteList(String requestURI) {
+        return !PatternMatchUtils.simpleMatch(whitelist, requestURI);
+    }
 }
--- diary/src/main/java/kr/ac/jejunu/diary/filter/config/WebConfig.java
+++ diary/src/main/java/kr/ac/jejunu/diary/filter/config/WebConfig.java
@@ -1,2 +1,23 @@
-package kr.ac.jejunu.diary.filter.config;public class WebConfig {
+package kr.ac.jejunu.diary.filter.config;
+
+import kr.ac.jejunu.diary.filter.ApiServiceFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.persistence.Basic;
+import javax.servlet.Filter;
+import java.net.PortUnreachableException;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public FilterRegistrationBean apiServiceFilter(){
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean=new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new ApiServiceFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+        return filterFilterRegistrationBean;
+    }
 }
