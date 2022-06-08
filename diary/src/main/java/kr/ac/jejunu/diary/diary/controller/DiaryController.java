9a6d06cc4ff369879b1714ed6223d850fcbfb8f3
caffeinejun <gurwns232e@gmail.com>
2022-06-08 09:32:32 +0900

    diary 주석 추가

--- diary/src/main/java/kr/ac/jejunu/diary/diary/controller/DiaryController.java
+++ diary/src/main/java/kr/ac/jejunu/diary/diary/controller/DiaryController.java
@@ -17,17 +17,20 @@ import java.util.List;
 public class DiaryController {
     private final DiaryService diaryService;
 
+//    다이어리 정보 가져오기 get
     @GetMapping("/api/user/diary")
     public List<AddDiaryResponseDto> getAllDiary(@Login User user){
         return diaryService.getAllDiary(user);
     }
 
+//    다이어리 작성 기능 post, 작성 글과 이미지 파일 입력 받음
     @PostMapping("/api/user/diary")
     public AddDiaryResponseDto addDiary(@Login User user, @RequestParam(name = "image",required = false) MultipartFile image,
                                         @RequestParam(name = "content") String contentMap){
         return diaryService.addDiary(user,image,contentMap);
     }
 
+//    다이어리 삭제 기능
     @DeleteMapping("/api/user/diary/{diaryId}")
     public ResponseDto deleteDiary(@PathVariable("diaryId")Long diaryId){
         diaryService.deleteDiary(diaryId);
--- diary/src/main/java/kr/ac/jejunu/diary/diary/service/DiaryService.java
+++ diary/src/main/java/kr/ac/jejunu/diary/diary/service/DiaryService.java
@@ -27,6 +27,7 @@ public class DiaryService {
     private final UserRepository userRepository;
     private final DiaryRepository diaryRepository;
 
+//    다이어리 추가 기능 작성한 글과 이미지를 저장, 이때 이미지는 로컬의 /images 경로에 저장
     @Transactional
     public AddDiaryResponseDto addDiary(User user, MultipartFile image, String content) {
         User user1 = userRepository.findById(user.getId()).get();
@@ -39,25 +40,6 @@ public class DiaryService {
         diary.addUser(user1);
         diaryRepository.save(diary);
         return new AddDiaryResponseDto(diary.getId(),diary.getImagePath(),diary.getContent());
-    }
-
-    @Transactional
-    public List<AddDiaryResponseDto> getAllDiary(User user) {
-        User user1 = userRepository.findById(user.getId()).get();
-        List<Diary> diaryArrayList = user1.getDiaryArrayList();
-        List<AddDiaryResponseDto> collect = diaryArrayList.stream().map(diary -> {
-            return new AddDiaryResponseDto(diary.getId(), diary.getImagePath(), diary.getContent());
-        })
-                .collect(Collectors.toList());
-        return collect;
-    }
-
-    @Transactional
-    public void deleteDiary(Long diaryId) {
-        Optional<Diary> byId = diaryRepository.findById(diaryId);
-        byId.orElseThrow(()->new IllegalArgumentException("다이어리가 없습니다."));
-        Diary diary = byId.get();
-        diaryRepository.delete(diary);
     }
 
     public String save(MultipartFile file) {
@@ -81,5 +63,26 @@ public class DiaryService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+//    유저의 다이어리 데이터를 배열 형태로 전부 가져옴
+    @Transactional
+    public List<AddDiaryResponseDto> getAllDiary(User user) {
+        User user1 = userRepository.findById(user.getId()).get();
+        List<Diary> diaryArrayList = user1.getDiaryArrayList();
+        List<AddDiaryResponseDto> collect = diaryArrayList.stream().map(diary -> {
+            return new AddDiaryResponseDto(diary.getId(), diary.getImagePath(), diary.getContent());
+        })
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+//    다이어리의 id 값을 입력 받으면 해당 다이어리는 삭제
+    @Transactional
+    public void deleteDiary(Long diaryId) {
+        Optional<Diary> byId = diaryRepository.findById(diaryId);
+        byId.orElseThrow(()->new IllegalArgumentException("다이어리가 없습니다."));
+        Diary diary = byId.get();
+        diaryRepository.delete(diary);
     }
 }
